d6170a4aa7d26a46e166943c7ceed4f6b461bee4
jpukabeals <pukab001@umn.edu>
2022-11-09 14:57:35 -0600

    Useful pilot study test that showed mold does impact NIR

--- nir_legacynet_mold-test.R
+++ nir_legacynet_mold-test.R
@@ -28,6 +28,150 @@
 
 # Import data -------------------------------------------------------------
 
-read.csv("nir_legacynet_20Sep2022.csv")
+read.csv("nir_legacynet_20Sep2022.csv") -> dat
+
+source("functions_nir.R")
+
+tidy.nir.report.with.periods(dat) -> dat_tidy
+
+library(stringr)
+
+dat_tidy %>% 
+  # glimpse()
+  dplyr::select(code) %>% 
+  mutate(plot = str_sub(code,1,2),
+         quality.num = str_sub(code,-1)) %>% 
+  mutate(quality.num = as.factor(quality.num)) %>% 
+  # glimpse()
+  mutate(quality.text = fct_recode(quality.num,
+                           "no mold" = "1",
+                           "some mold" = "2",
+                           "moldy" = "3")) %>% 
+  left_join(.,dat_tidy) -> dat2
+
+calc.rfq.rfv.grass(dat2) -> dat3
 
 
+# EDA ---------------------------------------------------------------------
+
+
+dat3 %>% 
+  ggplot(aes(rfq)) +
+  stat_bin()
+
+dat3 %>% 
+  ggplot(aes(rfq)) +
+  geom_boxplot()
+
+dat3 %>% 
+  ggplot(aes(rfq)) +
+  geom_boxplot(aes(fill=quality.text))
+
+dat3 %>% 
+  ggplot(aes(rfq)) +
+  geom_boxplot(aes(fill=quality.text)) +
+  facet_wrap(~plot)
+
+dat3 %>% 
+  ggplot(aes(quality.text,rfq)) +
+  stat_summary(aes(color=plot),
+               position = position_dodge(.5)) 
+
+
+# Graph output ------------------------------------------------------------
+source("ggplot_custom_theme.R")
+
+theme_set(theme_jpb.light())
+
+dat3 %>% 
+  ggplot(aes(quality.text,rfq)) +
+  stat_summary(aes(color=plot),
+               position = position_dodge(.5)) +
+  labs(x="",
+       y="Relative forage quality") +
+  theme(legend.title = element_text(size = 14),
+        legend.title.align = .5) 
+
+ggsave("rfq_legacynet-subsample.png",
+       dpi = 400,
+       width = 6,
+       height = 4,
+       units = "in")
+
+dat3 %>% 
+  ggplot(aes(quality.text,rfv)) +
+  stat_summary(aes(color=plot),
+               position = position_dodge(.5)) +
+  labs(x="",
+       y="Relative feed value") +
+  theme(legend.title = element_text(size = 14),
+        legend.title.align = .5)
+
+ggsave("rfv_legacynet-subsample.png",
+       dpi = 400,
+       width = 6,
+       height = 4,
+       units = "in")
+
+dat3 %>% 
+  ggplot(aes(quality.text,protein)) +
+  stat_summary(aes(color=plot),
+               position = position_dodge(.5)) +
+  labs(x="",
+       y="Protein (%, predicted)") +
+  theme(legend.title = element_text(size = 14),
+        legend.title.align = .5)
+
+ggsave("protein_legacynet-subsample.png",
+       dpi = 400,
+       width = 6,
+       height = 4,
+       units = "in")
+
+dat3 %>% 
+  ggplot(aes(quality.text,adf)) +
+  stat_summary(aes(color=plot),
+               position = position_dodge(.5)) +
+  labs(x="",
+       y="ADF (%, predicted)") +
+  theme(legend.title = element_text(size = 14),
+        legend.title.align = .5)
+
+ggsave("adf_legacynet-subsample.png",
+       dpi = 400,
+       width = 6,
+       height = 4,
+       units = "in")
+
+dat3 %>% 
+  ggplot(aes(quality.text,ndf)) +
+  stat_summary(aes(color=plot),
+               position = position_dodge(.5)) +
+  labs(x="",
+       y="NDF (%, predicted)") +
+  theme(legend.title = element_text(size = 14),
+        legend.title.align = .5)
+
+ggsave("ndf_legacynet-subsample.png",
+       dpi = 400,
+       width = 6,
+       height = 4,
+       units = "in")
+
+dat3 %>% 
+  ggplot(aes(quality.text,ndf48h)) +
+  stat_summary(aes(color=plot),
+               position = position_dodge(.5)) +
+  labs(x="",
+       y="NDFD 48h (%, predicted)") +
+  theme(legend.title = element_text(size = 14),
+        legend.title.align = .5)
+
+ggsave("ndfd48h_legacynet-subsample.png",
+       dpi = 400,
+       width = 6,
+       height = 4,
+       units = "in")
+
+dat3 %>% 
+  glimpse()
