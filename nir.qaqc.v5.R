6ecf51cff583aca418665545a53eca371f7e2486
jpukabeals <pukab001@umn.edu>
2023-02-07 09:11:13 -0600

    added in code for processing tli data, likely for james bowden, though it seems I also looked at some data from Caleb Anderson

--- nir.qaqc.v5.R
+++ nir.qaqc.v5.R
@@ -5,6 +5,98 @@
 # NIR data for Honken and Anderson 2021 from plots that were grazed
 
 
+# tli data ----------------------------------------------------------------
+
+
+tli.dat0 <- read_excel("Extensive Report_All-Products_20220119_0000_To_20220119_1449_avg.xlsx")
+
+tli.dat1 <- tli.dat0 %>%
+  mutate(datetime=`Date/Time of Analysis`,
+         code=`Sample ID`,
+         drymatter=`Dry matter %, Predicted`,
+         protein=`Protein As is %, Predicted`,
+         adf=`ADF As is %, Predicted`,
+         ndf=`NDF As is %, Predicted`,
+         ndf48h = `48dNDFr As is %, Predicted`,
+         lignin=`Lignin As is %, Predicted`) %>%
+  select(datetime, code, drymatter,protein,adf,ndf,ndf48h,lignin)
+
+
+# extract tli from sample recorded on 20210709
+
+tli.dat2 <- read_excel("Extensive Report_All-Products_20210601_0000_To_20210709_1017_avg.xlsx")
+
+tli.dat3 <- tli.dat2 %>%
+  mutate(datetime=`Date/Time of Analysis`,
+         code=`Sample ID`,
+         drymatter=`Dry matter %, Predicted`,
+         protein=`Protein As is %, Predicted`,
+         adf=`ADF As is %, Predicted`,
+         ndf=`NDF As is %, Predicted`,
+         ndf48h = `48dNDFr As is %, Predicted`,
+         lignin=`Lignin As is %, Predicted`) %>%
+  select(datetime, code, drymatter,protein,adf,ndf,ndf48h,lignin)
+str(tli.dat3)
+View(tli.dat3)
+# all 10 samples were taken on 6/24
+# tli.dat3 %>%
+#   filter(datetime %in% "6/24/2021")
+# using rowid to select
+
+tli.dat3 <- tli.dat3 %>%
+  rowid_to_column() 
+
+tli.dat3 <- tli.dat3 %>%
+  slice(706:735)
+
+# join the two datasets
+# make columns the same
+
+tli.dat3 <- tli.dat3 %>%
+  select(-rowid) 
+
+tli.dat4 <- bind_rows(tli.dat1,tli.dat3)
+
+# only using first observation for all sample id's for consistency
+tli.dat4 %>%
+  arrange(datetime,code) %>%
+  distinct(code,
+           .keep_all=T) %>%
+  arrange(datetime,code) %>%
+  View()
+
+tli.dat5 <- tli.dat4 %>%
+  arrange(datetime,code) %>%
+  distinct(code,
+           .keep_all=T) %>%
+  arrange(datetime,code)
+
+# splitting out code character
+tli.dat5 %>%
+  mutate(code.letter = str_sub(code,1,1),
+         code.number = str_sub(code,2,3)) %>%
+  select(code,code.letter,code.number)
+
+tli.dat6 <- tli.dat5 %>%
+  mutate(code.letter = str_sub(code,1,1),
+         code.number = str_sub(code,2,3),
+         experiment="TLI") %>%
+  relocate(code.letter,code.number,
+           .after=code) %>%
+  relocate(experiment,
+           .before=datetime)
+
+tli.dat6 %>%
+  arrange(code.letter,code.number) %>%
+  View()
+
+tli.dat <- tli.dat6 %>%
+  arrange(code.letter,code.number) %>%
+  select(-datetime)
+
+write.csv(tli.dat, "tli.nir.csv")
+
+
 # import ------------------------------------------------------------------
 
 library(tidyverse)
@@ -31,18 +123,16 @@ dat1 <- dat %>%
          ndf=`NDF As is %, Predicted`,
          ndf48h = `48dNDFr As is %, Predicted`,
          lignin=`Lignin As is %, Predicted`) %>%
-  filter(code>10000) %>%
-  dplyr::select(datetime, code, drymatter,protein,adf,ndf,ndf48h,lignin) 
+  # filter(code>10000) %>%
+  select(datetime, code, drymatter,protein,adf,ndf,ndf48h,lignin) 
 
 # Want to bring in plot number based on sample ID
 
 codes <- read_excel("onfarmgrazingcodes.xlsx")
-# codes <- codes %>%
-#   mutate(code=`Bottle Code`)
+codes <- codes %>%
+  mutate(code=`Bottle Code`)
 
 dat2 <- merge(codes,dat1, by="code")
-
-View(dat2)
 
 # want to confirm dataset is good
 # view(dat2)
@@ -66,7 +156,7 @@ dat3 <- dat2 %>%
             DMI=(-2.318)+(.442*CP)-(.01*CP^2)-(.0638*TDN)+(.000922*TDN^2)+(.18*ADF)-(0.00196*ADF^2)-(0.00529*CP*ADF),
             RFQ=DMI*TDN/1.23,
             RFV=DMI*((89.8-(0.779*ADF)))/1.29) %>%
-  dplyr::select(code,Year,Site,Experiment,Plot,Paddock,Treatment,datetime,
+  select(code,Year,Site,Experiment,Plot,Paddock,Treatment,datetime,
          RFQ,RFV,protein,adf,ndf)
 
 # clean up data by timestamps ---------------------------------------------
@@ -120,3 +210,7 @@ bwplot(RFV~factor(Treatment)|Site,dat3)
 bwplot(protein~factor(Treatment)|Site,dat3)
 bwplot(adf~factor(Treatment)|Site,dat3)
 bwplot(ndf~factor(Treatment)|Site,dat3)
+# RFQ and RFV seem higher in grazed plots at Anderson
+# ADF and NDF seem lower in grazed plots at Anderson...
+
+
