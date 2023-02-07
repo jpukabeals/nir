b2c19e194e07e41504657e1c1b2621c010a7cfbe
jpukabeals <pukab001@umn.edu>
2023-02-07 16:59:29 -0600

    quick report for Jake

---
+++ report_7Feb.R
@@ -0,0 +1,72 @@
+
+# Run above path
+# C:/Users/pukab001/Documents/R projects/nir/nir_import-tidy_7Feb2023.R
+
+source("nir_import-tidy_7Feb2023.R")
+
+dat %>% 
+  # colnames()
+  filter(ID2 == "RALLF") -> dat2
+
+dat2 %>% 
+  colnames()
+
+dat2 %>% 
+  ggplot(aes(CP)) +
+  stat_bin() +
+  labs(
+    caption = "CP range should be 10-20% for alfalfa"
+  )
+
+dat2 %>% 
+  ggplot(aes(NDF)) +
+  stat_bin() +
+  labs(
+    caption = "NDF range should be around 40"
+  )
+
+dat2 %>% 
+  ggplot(aes(ADF)) +
+  stat_bin() +
+  labs(
+    caption = "ADF should be around 25-30 for alfalfa"
+  )
+
+dat2 %>% 
+  ggplot(aes(rfq.legume)) +
+  stat_bin() +
+  labs(
+    caption = "RFQ should be around 125-225 for alfalfa, but it can up to 300"
+  )
+
+# I think we have super low ADF values which are causing our rfq values to be
+# super high
+
+# of RFQ>200, I bet ADF is lower than 20
+
+dat2 %>% 
+  dplyr::select(ADF,rfq.legume) %>% 
+  filter(rfq.legume>200)
+# yep, I was right
+
+# let's see if this is associated with a treatment
+
+dat2 %>% 
+  filter(rfq.legume>200) %>% 
+  distinct(ID9)
+
+# Seems the May24 cutting was the source of the super low ADF and high rfq
+# predictions
+
+dat2 %>% 
+  ggplot(aes(rfq.legume)) +
+  stat_bin(aes(fill=ID9))
+
+dat2 %>% 
+  ggplot(aes(ADF)) +
+  stat_bin(aes(fill=ID9))
+
+dat2 %>% 
+  ggplot(aes(CP)) +
+  stat_bin(aes(fill=ID9))
+# trend not as obvious for protein
