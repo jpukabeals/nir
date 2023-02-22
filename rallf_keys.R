6fd502227dad8395b83f646e92dde769b57ee41b
unknown <pukab001@umn.edu>
2023-02-22 12:46:57 -0600

    getting rid of a stupid old file
    adding in a new rallf key system that should work!

--- rallf_keys.R
+++ rallf_keys.R
@@ -2,17 +2,17 @@
 library(tidyverse)
 
 
-# TO DO 
-# finish applying bottle codes to 2022
-
 
 # Now I apply plot_harvest_year to the bottle codes
 
 
 
-
-
 # key1 plot treatments--------------------------------------------------------------------
+
+# read a csv in the nir folder
+
+
+
 
 # plot treatment key
 # https://docs.google.com/spreadsheets/d/14A64mmcHpf6ZhIt5dcRIOJ0lyA6a2RHpkwQKdTlLNII/edit#gid=1184137171
@@ -57,49 +57,73 @@ read.csv("rallf_key_plotTreatments.csv") -> key1
 
 # key 2 harvest code year ---------------------------------------------------
 
-rep(1:5,
-    each=4) -> cut
 
-# 20 is length
-
-rep(
-  c("st paul",
-  "rosemount"),
-  10
-) -> site
-
-rep(
-  c(
-    "35-day",
-    "45-day"
-  ),
-  each=2,
-  5
-) -> intensity
-
-seq(1,20,1) -> harvest_code
-
-tibble::tibble(
-  harvest_code,
-  site,
-  intensity,
-  cut
-) -> df1
-
-# now expand by year
-
-2021:2025 -> year
-
-
-df1 %>% 
-  expand_grid(year) %>% 
-  arrange(year) %>% 
-  relocate(year,.before=harvest_code) -> df2
-
-# manually add dates if desired
-# df2 %>% 
+# 
+# rep(1:5,
+#     each=4) -> cut
+# 
+# # 20 is length
+# 
+# rep(
+#   c("st paul",
+#   "rosemount"),
+#   10
+# ) -> site
+# 
+# rep(
+#   c(
+#     "35-day",
+#     "45-day"
+#   ),
+#   each=2,
+#   5
+# ) -> intensity
+# 
+# seq(1,20,1) -> harvest_code
+# 
+# tibble::tibble(
+#   harvest_code,
+#   site,
+#   intensity,
+#   cut
+# ) -> df1
+# 
+# # now expand by year
+# 
+# 2021:2025 -> year
+# 
+# 
+# df1 %>% 
+#   expand_grid(year) %>% 
+#   arrange(year) %>% 
+#   relocate(year,.before=harvest_code) -> df2
+# 
+# # manually add dates if desired
+# df2 %>%
 #   write.csv(
-#     "rallf_addDates.csv"
+#     "rallf_key_harvestCode.csv",
+#     row.names = F
 #   )
+# 
+# df2 -> key2
 
-df2 -> key2
+read.csv(
+  "rallf_key_harvestCode.csv"
+) -> key2
+
+
+
+# key code to harvest -----------------------------------------------------
+
+# https://docs.google.com/spreadsheets/d/1VY6EwF6AqK_G9cSJysfAWyfH9BS61m88rZPFHjsGmWs/edit#gid=99569789
+
+read.csv("rallf_key_codeHarvest.csv") -> key3
+
+
+# testing compatibility ---------------------------------------------------
+
+
+
+# key -- harvest_code to date ---------------------------------------------
+
+
