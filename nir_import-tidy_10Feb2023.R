fac2150d5b93e7554ae282d11eb813e16621b6a8
jpukabeals <pukab001@umn.edu>
2023-03-13 07:37:20 -0500

    deleting files I moved

--- nir_import-tidy_10Feb2023.R
+++
@@ -1,5 +0,0 @@
-# using nir_import-tidy_7Feb2023
-# this time I'm applying it to a master sheet of bottle codes from 2019-2022
-
-# if this works, next step is to just add all bottle codes to this sheet
-
